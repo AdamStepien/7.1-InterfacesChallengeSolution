b0c30628c2bcecaaf6924064f8b86a4831de17a3
AdamStepien <37138029+AdamStepien@users.noreply.github.com>
2018-03-12 19:46:55 +0100

    Add files via upload

---
+++ .gitignore/7.1 InterfacesChallenge - solution/src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player badyl = new Player("Badyl", 999,18);
+        System.out.println(badyl.toString());
+        saveObject(badyl);
+  //      loadObject(badyl);
+        System.out.println(badyl.toString());
+
+        ISavable chujołak = new Monster("Przejebanizor", 123,23);
+        System.out.println("Siła " + ((Monster) chujołak).getStrenght());
+        saveObject(chujołak);
+
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISavable objectToSave) {
+        for (int i = 0; i < objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to the storage device.");
+        }
+
+    }
+
+    public static void loadObject(ISavable objectToLoad) {
+        ArrayList values = readValues();
+        objectToLoad.read(values);
+    }
+}
---
+++ .gitignore/7.1 InterfacesChallenge - solution/src/com/company/Monster.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Monster implements ISavable{
+    private String name;
+    private int hitPoints;
+    private int strenght;
+
+    public Monster(String name, int hitPoints, int strenght) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strenght = strenght;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strenght=" + strenght +
+                '}';
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> values = new ArrayList<String>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strenght);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if (savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.strenght = Integer.parseInt(savedValues.get(2));
+        }
+
+    }
+}
